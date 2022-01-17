1e340b2448d28dffe3ed62c468bf39d94826b885
Kenad Araujo <kenadwanderson@gmail.com>
2022-01-16 22:59:38 -0300

    -Construindo um preview

--- app/src/main/java/com/polligonalApps/openomr/CameraPreview.java
+++ app/src/main/java/com/polligonalApps/openomr/CameraPreview.java
@@ -1,5 +1,6 @@
 package com.polligonalApps.openomr;
 
+import android.app.Application;
 import android.content.Context;
 import android.hardware.Camera;
 import android.util.Log;
@@ -7,51 +8,16 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.NonNull;
+import androidx.camera.camera2.Camera2Config;
+import androidx.camera.core.CameraXConfig;
 
 import java.io.IOException;
 
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
-    private SurfaceHolder holder;
-    private Camera camera;
+public class CameraPreview extends Application implements CameraXConfig.Provider {
 
-    public CameraPreview(Context context, Camera camera) {
-        super(context);
-        this.camera = camera;
-        holder = getHolder();
-        holder.addCallback(this);
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-    }
-
+    @NonNull
     @Override
-    public void surfaceCreated(@NonNull SurfaceHolder holder) {
-        try {
-            this.camera.setPreviewDisplay(this.holder);
-            this.camera.startPreview();
-        } catch (IOException e) {
-            Log.d("CAMERA", "Error setting camera preview: " + e.getMessage());
-        }
-    }
-
-    @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
-        if(this.holder == null ){
-            return;
-        }
-        try{
-            this.camera.stopPreview();
-        }catch (Exception e){
-            Log.d("CAMERA", "Error setting camera preview: " + e.getMessage());
-        }
-        try{
-            this.camera.setPreviewDisplay(this.holder);
-            this.camera.startPreview();
-        }catch (Exception e){
-            Log.d("CAMERA", "Error setting camera preview: " + e.getMessage());
-        }
+    public CameraXConfig getCameraXConfig() {
+        return Camera2Config.defaultConfig();
     }
 }
--- app/src/main/java/com/polligonalApps/openomr/MainActivity.java
+++ app/src/main/java/com/polligonalApps/openomr/MainActivity.java
@@ -1,36 +1,110 @@
 package com.polligonalApps.openomr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.Context;
+import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.os.Bundle;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Camera camera;
-    private CameraPreview cameraPreview;
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
+
+    private ExecutorService cameraExecutor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.camera = getInstanciaCamera();
+        cameraExecutor = Executors.newSingleThreadExecutor();
 
-        this.cameraPreview = new CameraPreview(this,this.camera);
-        FrameLayout preview = findViewById(R.id.camera_preview);
-        preview.addView(this.cameraPreview);
+        iniciarCamera();
+
+        Button enableCamera = findViewById(R.id.camera_capture_button);
+        enableCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (temPermissaoParaCamera()) {
+                    capturarFoto();
+                } else {
+                    pedirRequisicao();
+                }
+            }
+        });
     }
 
-    private boolean verificaSeACameraExiste(Context contexto){
-        return contexto.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
     }
-    private Camera getInstanciaCamera(){
-        Camera c = null;
-        c = Camera.open();
-        return c;
+
+    private boolean temPermissaoParaCamera() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void pedirRequisicao() {
+        ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
+    }
+
+    private void iniciarCamera(){
+        ListenableFuture<ProcessCameraProvider> camerasProviderFuture =
+                ProcessCameraProvider.getInstance(this);
+        camerasProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider cameraProvider = camerasProviderFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+    }
+
+    private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        PreviewView previewView = findViewById(R.id.viewFinder);
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        Camera camera = cameraProvider.bindToLifecycle(this,cameraSelector,preview);
+    }
+
+    private void capturarFoto() {
+
     }
 }
