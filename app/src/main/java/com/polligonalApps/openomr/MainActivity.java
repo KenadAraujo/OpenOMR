3a55041115404f6427077ef27d90c7beec61353e
Kenad Araujo <kenadwanderson@gmail.com>
2022-01-18 17:56:39 -0300

    -Tirando a foto

--- app/src/main/java/com/polligonalApps/openomr/MainActivity.java
+++ app/src/main/java/com/polligonalApps/openomr/MainActivity.java
@@ -6,32 +6,44 @@ import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Size;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private static final int CAMERA_REQUEST_CODE = 10;
-
     private ExecutorService cameraExecutor;
+    String currentPhotoPath;
+    ImageCapture imageCapture = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (temPermissaoParaCamera()) {
-                    capturarFoto();
+                    try {
+                        capturarFoto();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 } else {
                     pedirRequisicao();
                 }
@@ -103,14 +119,58 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        ImageCapture imageCapture = new ImageCapture.Builder()
+        imageCapture = new ImageCapture.Builder()
                 .setTargetRotation(previewView.getDisplay().getRotation())
                 .build();
 
-        Camera camera = cameraProvider.bindToLifecycle(this,cameraSelector,imageCapture,preview);
+        ImageAnalysis imageAnalysis =
+                new ImageAnalysis.Builder()
+                        // enable the following line if RGBA output is needed.
+                        //.setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                        .setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+        imageAnalysis.setAnalyzer(cameraExecutor, new ImageAnalysis.Analyzer() {
+            @Override
+            public void analyze(@NonNull ImageProxy imageProxy) {
+                int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
+                // insert your code here.
+                //...
+                // after done, release the ImageProxy object
+                imageProxy.close();
+            }
+        });
+        cameraProvider.bindToLifecycle(this,cameraSelector,imageCapture, imageAnalysis,preview);
+
     }
 
-    private void capturarFoto() {
+    private void capturarFoto() throws IOException {
 
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(criarArquivoImagem()).build();
+        imageCapture.takePicture(outputFileOptions, cameraExecutor,
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                        System.out.println("=======Imagem salva");
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        exception.printStackTrace();
+                    }
+                });
+    }
+    private File criarArquivoImagem() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "GABARITO_" + timeStamp + "_";
+        File file = new File(this.getExternalFilesDir("jpg"), imageFileName+ ".jpg");
+
+        if(!file.exists())
+            file.mkdirs();
+        currentPhotoPath = file.getPath().toString();
+        return file;
     }
 }
